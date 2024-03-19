f97c5e77f8e37131f11d8bc4b38a5a68c5cfb70a
mephisto <pikalov.ea97@gmail.com>
2024-03-19 03:42:07 +0300

    sdgsdgs

--- calculator.js
+++ calculator.js
@@ -1,30 +1,86 @@
 const output = document.querySelector('.output');
-const buttons = document.querySelector('.grid');
+const numbers = document.querySelector('.grid');
+const precent = document.getElementById('precent');
 const plus = document.getElementById('plus');
 const minus = document.getElementById('minus');
 const multiply = document.getElementById('multiply');
 const division = document.getElementById('division');
 const equal = document.getElementById('equal');
-const deleteAll = document.getElementById('clearAll');
-const deleteLastNum = document.getElementById('clearLastNum');
+const reset = document.getElementById('reset');
+const resetLastNum = document.getElementById('resetLastNum');
 
-let firstNum = '';
-let secondNum = '';
-let firstNumFlag = true;
-let operator;
-
-deleteAll.addEventListener('click', clearAll);
-deleteLastNum.addEventListener('click',deleteLastNumber);
-buttons.addEventListener('click',getNumber);
+reset.addEventListener('click', getResetInOutput);
+resetLastNum.addEventListener('click',resetLastNumber);
+precent.addEventListener('click',getPrecentOfNumber);
+numbers.addEventListener('click',getNumber);
 plus.addEventListener('click', useOperator);
 minus.addEventListener('click', useOperator);
 multiply.addEventListener('click', useOperator);
 division.addEventListener('click', useOperator);
 equal.addEventListener('click', useOperatorEqual);
 
+let firstNum = '';
+let secondNum = '';
+let firstNumFlag = true;
+let operator = '';
+
+function getNumber(e){
+    if(e.target.value === '' || e.target.classList.contains('grid')) return;
+    if(output.textContent[0] === '0') output.textContent = '';
+    toggleStyleActiveBtn();
+    if(firstNumFlag){
+        firstNum += e.target.value;
+        getBeautifulOutput(firstNum);
+    }
+    else{
+        secondNum += e.target.value;     
+        getBeautifulOutput(secondNum);
+    };
+};
+
+function getBeautifulOutput(number){
+    const numberString = String(number);
+    const numbers = [];
+    let count = -1;
+    for(let i = numberString.length-1; i >= 0; i--){
+        count++ 
+        if(numberString[i] === '.'){
+            count = -1;
+        }
+        if(4 % count === 1){
+            numbers.push(' ');
+            count = 0;
+        }
+        numbers.push(numberString[i]); 
+    };
+    numbers.reverse();
+    output.textContent = numbers.join('');
+};
+
+function getResultInOutput(){
+    if(firstNum && secondNum){
+        switch (operator) {
+            case '+': 
+                getBeautifulOutput(Number(firstNum) + Number(secondNum))
+                break;
+            case '-':
+                getBeautifulOutput(Number(firstNum) - Number(secondNum))
+                break;
+            case '×':
+                getBeautifulOutput(Number(firstNum) * Number(secondNum))
+                break;
+            case '÷':
+                getBeautifulOutput(Number(firstNum) / Number(secondNum))
+                break;
+        };
+        firstNum = Number(output.textContent.replaceAll(' ',''));
+        secondNum = '';
+    }  
+};
+
 function useOperator(e){
-    deleteStyleActiveBtn();
-    e.target.classList.add('active');   
+    toggleStyleActiveBtn();
+    e.target.classList.add('active-operator');   
     operator = e.target.textContent;
     if(!firstNum && !secondNum){
         firstNumFlag = true;
@@ -33,81 +89,44 @@ function useOperator(e){
         firstNumFlag = false;
     }
     return getResultInOutput();
-}
-
-function getResultInOutput(){
-    if(firstNum && secondNum){
-        switch (operator) {
-            case '+':
-                output.textContent = firstNum + secondNum;
-                break;
-            case '-':
-                output.textContent = firstNum - secondNum;
-                break;
-            case '×':
-                output.textContent = firstNum * secondNum;
-                break;
-            case '÷':
-                output.textContent = firstNum / secondNum;
-                break;
-            default:
-                break;
-        };
-        firstNum = Number(output.textContent);
-        secondNum = '';
-    }  
-}
-
-function getNumber(e){
-    const target = e.target.textContent;
-    if(['+','-','×','÷','=','÷','AC','C','%','%',].includes(target) || e.target.classList.contains('grid')) return;
-    if (output.textContent[0] === '0') output.textContent = '';
-    output.textContent += target;
-    deleteStyleActiveBtn();
-    
-    if(firstNumFlag){
-        firstNum += target;
-        firstNum = Number( firstNum );
-    }
-    else{
-        secondNum += target;
-        output.textContent = '';
-        output.textContent = secondNum;
-        secondNum = Number( secondNum );
-    };
 };
 
-
-function clearAll(){
-    deleteStyleActiveBtn();
-    output.textContent = '0'
+function getResetInOutput(){
+    toggleStyleActiveBtn();
+    output.textContent = 0;
     firstNum = '';
     secondNum = '';
-    firstNumFlag = true;
     operator = '';
-}
-function deleteStyleActiveBtn(){
-    const activeClass = document.querySelector('.active');
+    firstNumFlag = true;
+};
+
+function getPrecentOfNumber(){
+    if(!firstNumFlag){
+        output.textContent = Number(firstNum) * Number(secondNum) / 100;
+    }
+};
+function toggleStyleActiveBtn(){
+    const activeClass = document.querySelector('.active-operator');
     if(activeClass){
-        activeClass.classList.remove('active');
+        activeClass.classList.remove('active-operator');
     };
 };
 function useOperatorEqual(){
-    deleteStyleActiveBtn();
+    toggleStyleActiveBtn();
     getResultInOutput();
-}
+};
 
-function deleteLastNumber(){
-    deleteStyleActiveBtn();
+function resetLastNumber(){
+    toggleStyleActiveBtn();
     if(firstNum && !secondNum){
         firstNumFlag = true;
-        output.textContent='0';
+        output.textContent = '0';
         firstNum = '';
         secondNum = '';
     }
     else if(firstNum && secondNum){
         firstNumFlag = false;
-        output.textContent=firstNum;
+        output.textContent = firstNum;
         secondNum = '';
-    }
-}
+    };
+};
